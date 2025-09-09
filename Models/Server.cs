98cf4d60561695136041554e9810ea3cd18aeac7
vijay-jeevanandam-man <vijay.jeevanandam@man.com>
2025-09-09 15:47:24 -0400

    Add project files.

---
+++ Models/Server.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorIntroduction.Models
+{
+    public class Server
+    {
+        public Server() 
+        { 
+            Random random = new Random();
+            int randomNumber = random.Next(0, 2);
+            IsOnline = randomNumber != 0;
+        }
+        public int ServerId { get; set; }
+        public bool IsOnline { get; set; }
+        public string? Name { get; set; }
+        public string? City { get; set; }
+    }
+}
