d01cc6f0214824c2b7b4032dd7a3b3bf34f43f1f
vijay-jeevanandam-man <vijay.jeevanandam@man.com>
2025-09-11 12:14:12 -0400

    add servers list and city buttons

---
+++ Models/CitiesRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorIntroduction.Models
+{
+    public class CitiesRepository
+    {
+        private static List<string> cities = new List<string>()
+        {
+            "Toronto",
+            "Montreal",
+            "Ottawa",
+            "Calgary",
+            "Halifax"
+        };
+
+        public static List<string> GetCities() => cities;
+    }
+}
